e8ee0c7a26fd114bb8064345966a9d0abe979c64
gwadson <guimatsa@hotmail.com>
2020-07-21 06:11:26 +0200

    change footer and remove github icon

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,16 +3,17 @@ import './Footer.css'
 
 export default () => (
   <div>
-    <h2 className="taCenter">
+    {/* <h2 className="taCenter">
       Follow us{' '}
       <a href="https://instagram.com/thrivegoldcoast/">@thrivegoldcoast</a>
-    </h2>
+    </h2> */}
     <br />
     <footer className="footer">
       <div className="container taCenter">
         <span>
-          © Copyright {new Date().getFullYear()} All rights reserved. Crafted by{' '}
-          <a href="https://thriveweb.com.au/">Thrive</a>.
+          Writen by <a href="https://thriveweb.com.au/">Wadson Guimatsa</a>
+          {/* © Copyright {new Date().getFullYear()} All rights
+          reserved. Writen by <a href="https://thriveweb.com.au/">Wadson Guimatsa</a>. */}
         </span>
       </div>
     </footer>
